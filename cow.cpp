1dcc3d440be9c1e1830ed6e353c78b0b3cea893c
Zubair1021 <121050120+Zubair1021@users.noreply.github.com>
2023-01-10 14:41:47 +0500

    Add files via upload

---
+++ cow.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+main(){
+cout<<"            - - - - - - - - - - - - - - - - - - - - - - - - - - - - -                                           "<<endl;
+cout<<"                                                                                                  "<<endl;
+cout<<"                         o    ^___^                                                                 "<<endl;
+cout<<"                                                                                                  "<<endl;
+cout<<"                         o    (o o) \_________________                                                         "<<endl;
+cout<<"                                &                                                                      "<<endl;
+cout<<"                              (___) \      C O W      )\/\                                                                           "<<endl;
+cout<<"                                                                                                                     "<<endl;
+cout<<"                                     | | - - - - - W  |                                                                    "<<endl;
+cout<<"                                                                                                         "<<endl;
+cout<<"                                     | |            | |                                                                    "<<endl;
+system("color 17");
+
+
+}
